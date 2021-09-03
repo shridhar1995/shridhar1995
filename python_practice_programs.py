a4ad298657cd48adc7a7456111f12ae42e5e84dd
shridhar1995 <90026515+shridhar1995@users.noreply.github.com>
2021-09-03 12:19:17 +0530

    Create python_practice_programs.py

---
+++ python_practice_programs.py
@@ -0,0 +1,6 @@
+#Program that tells you the year you will turn 100 years old
+name = input('Enter your Name :: ')
+age = int(input('Enter your age :: '))
+
+n = int(input('Enter a number :: '))
+print(n * (name + ' ,you will turn 100 years old in ' + str(100-age+2021) + '\n'))
